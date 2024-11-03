84a045b4490cb377a391d87fe895ce40538dfb15
tauheed0110 <tauheed0110@gmail.com>
2024-11-03 12:06:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-const arr = [1, 2, 3, 4, 1, 0, 2, 2];
+const arr = [4, 3, 2, 1];
 
 const divide = (arr, n) => {
   // Write your code here
+  n = Number(n);
+  let ans = [];
+  let start=0;
+  
+  while(start < arr.length){
+    let temp = [];
+    let sum = 0;
+    while(start < arr.length && sum <= n){
+        sum += arr[start];
+        if(sum <= n){
+            temp.push(arr[start]);
+            start++;
+        }
+    }
+    if(temp.length > 0)
+        ans.push(temp);
+  }
+  start++;
+  return ans;
 };
 
 const n = prompt("Enter n: ");
